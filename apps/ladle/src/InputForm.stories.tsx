00661a515e811329f8f47a5174fa9655fa6a7234
David Taylor <114563+tayles@users.noreply.github.com>
2026-02-14 17:01:56 -0500

    chore: Stories

---
+++ apps/ladle/src/InputForm.stories.tsx
@@ -0,0 +1,11 @@
+import type { Story } from '@ladle/react';
+
+import {
+  InputForm,
+} from '@git-repo-analyzer/ui';
+
+export default {
+  title: 'Components/InputForm',
+};
+
+export const Default: Story = () => (<InputForm/>);
