13912c1490644df3122e93cdcf3e002afbe146db
David Taylor <114563+tayles@users.noreply.github.com>
2026-02-15 07:41:37 -0500

    feat: Theme switching

--- apps/ladle/.ladle/components.tsx
+++ apps/ladle/.ladle/components.tsx
@@ -1,11 +1,20 @@
 import type { GlobalProvider } from '@ladle/react';
 
 import './ladle.css';
+import { useEffect } from 'react';
 
 /**
  * Global provider that wraps all stories
  * This is where you add global styles, providers, etc.
  */
-export const Provider: GlobalProvider = ({ children }) => {
+export const Provider: GlobalProvider = ({ children, globalState }) => {
+  useEffect(() => {
+    if (globalState.theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [globalState.theme]);
+
   return children;
 };
