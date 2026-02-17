3f91025d323571fbc28da47e244c3e28605cf285
David Taylor <114563+tayles@users.noreply.github.com>
2026-02-17 04:51:22 -0500

    build: Copy root readme

--- apps/cli/bunup.config.ts
+++ apps/cli/bunup.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'bunup';
+import { copy } from 'bunup/plugins';
 
 export default defineConfig({
   noExternal: ['@git-repo-analyzer/core'],
+  // Copy the root README.md file to the output directory
+  plugins: [copy(['../../README.md']).to('README.md')],
 });
