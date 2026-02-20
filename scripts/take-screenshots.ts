4f51526806352dd59eb4b9131364021b4bea7754
David Taylor <114563+tayles@users.noreply.github.com>
2026-02-19 23:55:26 -0500

    feat: Script to generate screenshots

--- scripts/take-screenshots.ts
+++ scripts/take-screenshots.ts
@@ -1,10 +1,15 @@
+/**
+ * This script is used to take screenshots of the CLI and website in action, for use in the README.
+ */
+
 import { $ } from 'bun';
 
 const repo = 'facebook/docusaurus';
 
+const cliScreenshotPath = 'docs/screenshots/git-repo-analyzer-cli-screenshot.png';
+
 // Use https://github.com/homeport/termshot to take screenshots of the CLI in action
-await $`termshot -- bun run apps/cli/src/cli.ts ${repo}`;
+await $`termshot --filename ${cliScreenshotPath} -- bun run apps/cli/src/cli.ts ${repo}`;
 
 // Use playwright to take screenshots of the website in action
-
 // TODO
