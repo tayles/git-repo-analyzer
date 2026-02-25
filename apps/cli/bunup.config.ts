ad6e7f1573fc21dd6bd9b73922e2183cc2066ba8
David Taylor <114563+tayles@users.noreply.github.com>
2026-02-25 01:59:46 -0500

    build: Do not copy readme to dist dir

--- apps/cli/bunup.config.ts
+++ apps/cli/bunup.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'bunup';
-import { copy } from 'bunup/plugins';
 
 export default defineConfig({
   noExternal: ['@git-repo-analyzer/core'],
-  // Copy the root README.md file to the output directory
-  plugins: [copy(['../../README.md']).to('README.md')],
 });
