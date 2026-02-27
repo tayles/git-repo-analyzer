79097084dcb7b54204cc58727c72e8aa2cd70fab
David Taylor <114563+tayles@users.noreply.github.com>
2026-02-26 21:21:01 -0500

    feat: Add 48x48px icon

--- apps/extension/manifest.config.ts
+++ apps/extension/manifest.config.ts
@@ -5,13 +5,24 @@ import pkg from './package.json';
 export default defineManifest({
   manifest_version: 3,
   name: 'Git Repo Analyzer',
-  description: 'View the tech stack, health and other insights of any GitHub repository',
+  description:
+    'View the tech stack, work patterns, project structure and health of any GitHub repository',
   version: pkg.version,
   icons: {
+    16: 'public/icons/git-repo-analyzer-icon-16x16.png',
+    32: 'public/icons/git-repo-analyzer-icon-32x32.png',
+    48: 'public/icons/git-repo-analyzer-icon-48x48.png',
+    96: 'public/icons/git-repo-analyzer-icon-96x96.png',
+    128: 'public/icons/git-repo-analyzer-icon-128x128.png',
     512: 'public/icons/git-repo-analyzer-icon-512x512.png',
   },
   action: {
     default_icon: {
+      16: 'public/icons/git-repo-analyzer-icon-16x16.png',
+      32: 'public/icons/git-repo-analyzer-icon-32x32.png',
+      48: 'public/icons/git-repo-analyzer-icon-48x48.png',
+      96: 'public/icons/git-repo-analyzer-icon-96x96.png',
+      128: 'public/icons/git-repo-analyzer-icon-128x128.png',
       512: 'public/icons/git-repo-analyzer-icon-512x512.png',
     },
     default_title: 'Git Repo Analyzer',
